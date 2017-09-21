84ad3c5744f3f30f572a33b2c9e02d8163b34c4c
akshaypatidar <akshaypatidar1999@gmail.com>
2017-09-22 05:15:05 +0530

    Student Login

--- app/src/main/java/com/example/dhruv/getplaced/MainActivity.java
+++ app/src/main/java/com/example/dhruv/getplaced/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.dhruv.getplaced;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button studentButton,companyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        studentButton = (Button) findViewById(R.id.student_login);
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Student_login.class));
+            }
+        });
+
+        companyButton=(Button) findViewById(R.id.company_login);
+        companyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this,Company_login.class));
+            }
+        });
     }
 }
